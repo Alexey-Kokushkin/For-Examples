666d90fed9d5486066a90acb2cc3a74946d7b4a5
Alexey-Kokushkin <kokushkin_83@mail.ru>
2022-02-25 21:57:52 +0900

    Example005_If_ELSE

---
+++ Example005_IF_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя, ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Урраа, это же Маша!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
